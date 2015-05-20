ec61b7865fe952541b96df6265ed9dee448f79cc
Mibizina <andrainamihary@gmail.com>
2015-05-20 02:14:33 +0200

    Elgamal jusqu'au question 4

--- Elgamal/src/chiffrement/elgamal/Elgamal.java
+++ Elgamal/src/chiffrement/elgamal/Elgamal.java
@@ -5,6 +5,9 @@
  */
 package chiffrement.elgamal;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.math.BigInteger;
 import java.util.Random;
 
@@ -36,18 +39,28 @@ public class Elgamal {
              return p;
             }
     }
+    //Ordre du nombre a dans Z/nZ
+    public static BigInteger ordre(BigInteger a, BigInteger n)
+    {
+        for (BigInteger i = BigInteger.ONE; i.compareTo(n) < 0; i.add(BigInteger.ONE))
+        {
+            if(a.modPow(i, n).equals(BigInteger.ONE))
+                return i;
+        }
+        return BigInteger.ZERO;
+    }
     
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Random mohamed = new Random();
         Elgamal k = new Elgamal(512, 40, mohamed);
         BigInteger p = k.getPrime(512, 40, mohamed);
         System.out.println();
         System.out.println("on a un nombre premier p = ");
         System.out.println (p);
-        BigInteger g;
+        BigInteger g, g2, gpp, g2pp, pPrime, pPrime2, gpp2;
         do 
         {
             System.out.println(".");
@@ -60,7 +73,28 @@ public class Elgamal {
         while (p.compareTo(g)<0);
         System.out.println("\non a un nombre premier g = ");
         System.out.println (g);
-        System.out.println(p.subtract(g));
+        //System.out.println(p.subtract(g));
+        g2 = g.modPow(BigInteger.ONE.add(BigInteger.ONE), p);//calcul de g² modulo p
+        System.out.println("g² = " + g2);
+        pPrime  = p.subtract(BigInteger.ONE).divide(new BigInteger("2"));//calcul de p' à partir de p ==>p' = (p-1)/2
+        gpp = g.modPow(pPrime, p); //calcul de g^p' modulo p
+        System.out.println("p' = " + pPrime);
+        System.out.println("g^p' = " + gpp);
+        g2pp = g.modPow(pPrime.multiply(new BigInteger("2")), p);//calcul de g^2p' modulo p
+        System.out.println("g^2p' = " + g2pp);
+        
+        //ecriture dans un fichier texte
+        String nomfichier;
+        nomfichier = "Elgamal.txt";
+        PrintWriter sortie = new PrintWriter(new FileWriter(nomfichier));
+        sortie.println("\"on a un nombre premier p = \"" + p);//sortie.println(X) pour ecrire X dans le fichier texte
+        sortie.println("p' = " + pPrime);
+        sortie.println("on a un nombre premier g = " + g );
+        sortie.println("g² = " + g2);
+        sortie.println("g^p' = " + gpp);
+        sortie.println("g^2p' = " + g2pp);
+        sortie.close();//pour terminer l'écriture
+        
     }
     
 }
